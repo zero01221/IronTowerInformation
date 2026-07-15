1aa015274fd7589b701d51f7b0010a2cadf07df7
zero01221 <143979558+zero01221@users.noreply.github.com>
2026-07-15 10:33:55 +0800

    Update __init__.py

--- scripts/bidding_scraper/crawlers/__init__.py
+++ scripts/bidding_scraper/crawlers/__init__.py
@@ -2,25 +2,17 @@
 """爬虫模块"""
 
 from .ccgp import CcgpCrawler
-<<<<<<< HEAD
 from .ccgp_yunnan import CcgpYunnanCrawler
 from .cebpubservice import CebpubserviceCrawler
 from .chinatowercom import ChinaTowerComCrawler
 from .tower_com_cn import TowerComCnCrawler
 from .miit_txzbqy import MiitTxzbqyCrawler
-=======
-from .ynggzy import YnggzyCrawler
->>>>>>> 117299cc0ac4358d094eb4bce3dbaa9ede3c28aa
 
 __all__ = [
     "CcgpCrawler",
-<<<<<<< HEAD
     "CcgpYunnanCrawler",
     "CebpubserviceCrawler",
     "ChinaTowerComCrawler",
     "TowerComCnCrawler",
     "MiitTxzbqyCrawler",
-=======
-    "YnggzyCrawler",
->>>>>>> 117299cc0ac4358d094eb4bce3dbaa9ede3c28aa
 ]
